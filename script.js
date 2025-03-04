02f74ea4f2f50f4e19cec7f5b6564a08241af451
abbycute27 <cabbalda22@src.pshs.edu.ph>
2025-03-03 22:59:39 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 function buttonAko() {
-let message = prompt('Enter the message:');
+let sentMessage = prompt('Enter the message:');
 
 
-if ((message.indexOf('FR') >= 0) && (message.indexOf('AI') >=0)) {
-document.getElementById('result').innerHTML = `The message "${message}" is legitimate!`;
+if ((sentMessage.indexOf('FR') >= 0) && (sentMessage.indexOf('AI') >=0)) {
+document.getElementById('result').innerHTML = `The message "${sentMessage}" is legitimate!`;
 }
-else if (message.indexOf('AI') >= 0|| message.indexOf('aI') >= 0 || message.indexOf('Ai') >= 0) {
-document.getElementById('result').innerHTML = `The message "${message}" is tampered with!`;
+else if (sentMessage.indexOf('AI') >= 0|| sentMessage.indexOf('aI') >= 0 || sentMessage.indexOf('Ai') >= 0) {
+document.getElementById('result').innerHTML = `The message "${sentMessage}" is tampered with!`;
 }
-else if (message.indexOf('FR') >= 0){
-document.getElementById('result').innerHTML = `The message "${message}" is legitimate!`;
+else if (sentMessage.indexOf('FR') >= 0){
+document.getElementById('result').innerHTML = `The message "${sentMessage}" is legitimate!`;
 }
 else {
-document.getElementById('result').innerHTML = `The message "${message}" is yet to be encoded!`;
+document.getElementById('result').innerHTML = `The message "${sentMessage}" is yet to be encoded!`;
 }
 }
